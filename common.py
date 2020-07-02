b09388d66d89c85f3d59bfeb1997bd2fb027f631
Félix Faisant <xcodexif@xif.fr>
2020-07-02 23:59:12 +0200

    langevin surival proba without demispace assumption; periodic reseting; common langevin survival code

---
+++ common.py
@@ -0,0 +1,125 @@
+import numpy as np
+from math import *
+π = np.pi
+
+import scipy.special as ss
+import scipy.integrate as sint
+import mpmath
+
+
+def convolve_around_center (func1, func2, N1, Nout, Δx, x_center=0):
+	u"""
+	Convolve two functions func1 and func2, with func1 decreasing away from 0 (convolution kernel) :
+	(func1*func2)(x) = ∫ dx1 func2(x-x1) func1(x1)
+					 ≃ Δx ∑ func2(x-k⋅Δx) func1(k⋅Δx) from k=-N1 to +N1
+	-> Only 2⋅N1+1 points of func1 are sampled around 0, while func2 is evaluated as needed (around x=x_center).
+	The result is ( x, (func1*func2)(x) ) with X = [x_center-Nout⋅Δx, x_center+Nout⋅Δx].
+	Typically, Nout << N1. Nout can be 0, and in that case, the result is simply (func1*func2)(x_center).
+	"""
+	# samples of func1
+	X1 = np.linspace(-N1*Δx, +N1*Δx, 2*N1+1)
+	Y1 = func1( X1 )
+	# samples of func2
+	X2 = x_center + np.linspace((-N1-Nout)*Δx, (+N1+Nout)*Δx, 2*(N1+Nout)+1)
+	Y2 = func2( X2 )
+	# output
+	Conv_x = x_center + np.linspace(-Nout*Δx, +Nout*Δx, 2*Nout+1)
+	Conv = np.zeros(2*Nout+1)
+	for i in range(2*Nout+1):
+		# pas optimal car ré-évaluation inutile de func2 :
+		#  Y2 = func2( Conv_x[i] - X1 )
+		#  Conv[i] = np.sum( Y1 * Y2 ) * Δx
+		# mieux :
+		Y2loc = Y2[i:i+2*N1+1]
+		Conv[i] = np.sum( Y1 * Y2loc ) * Δx
+	return Conv_x, Conv
+
+def distr_x0_harmonic (x, σ):
+	return np.exp(-(x/σ)**2/2)/sqrt(2*π)/σ
+
+# No resetting
+
+def fpt_free_survival (L, t, D, σ):
+	survdist_σ0 = lambda x, t: ss.erf( x/np.sqrt(4*D*t) ) * np.heaviside(x,0.5)
+	if σ == 0:
+		return survdist_σ0(L,t)
+	else:
+		assert np.isscalar(L)
+		distr_x0 = lambda x: distr_x0_harmonic(x, σ)
+		def ps (t):
+			surv_f = lambda x: survdist_σ0(x, t)
+			return convolve_around_center(distr_x0, surv_f, x_center=L, N1=1000+int(500*sqrt(4*D*np.max(t))/σ), Nout=0, Δx=0.01*σ)[1]
+		return np.vectorize(ps)(t)
+
+#def fpt_free_tau (t, x_targ):
+#	if σ == 0:
+#		return x_targ/(2*np.sqrt(π*D*t**3)) * np.exp(-x_targ**2/(4*D*t))
+#	else:
+#		pass
+
+# Poissonian reset
+
+def fpt_poisson_c (α, D, L):
+	return sqrt(α/D)*L
+
+def fpt_poisson_survival_σ0 (x, t, α, D):
+	x = np.atleast_1d(x)
+	t = np.atleast_1d(t)
+	P = np.zeros((len(x),len(t)))
+	for i in range(len(x)):
+		def psr_lp (s):
+			kappa = mpmath.sqrt( (α + s) / D )
+			e = mpmath.exp(-kappa * x[i])
+			return (1 - e) / (s + α*e)
+		for j in range(len(t)):
+			if x[i] < 0:
+				P[i,j] = 0
+			else:
+				P[i,j] = mpmath.invertlaplace(psr_lp, t[j], method='talbot')
+	return np.squeeze(P)
+
+def fpt_poisson_tau (b, c):
+	if np.all(np.isinf(b)):
+		return 4/c**2 * ( np.exp(c) - 1 )
+	else:
+		return 4/c**2 * ( (2*np.exp(-c**2/2/b**2)) / ( np.exp(c)*ss.erfc((c/b+b)/sqrt(2)) + np.exp(-c)*ss.erfc((c/b-b)/sqrt(2)) ) - 1 )
+
+# Periodical reset
+
+def fpt_periodical_c (rT, D, L):
+	return L/sqrt(4*D*rT)
+
+def fpt_periodic_tau (b, c):
+	if np.all(np.isinf(b)):
+		return ( ss.erf(c) + 2*c*(np.exp(-c**2)/sqrt(π)-c*ss.erfc(c)) ) / ss.erfc(c) / c**2
+	else:
+		int_exp_erf = lambda v,b,c: sint.quad( lambda u, v,b,c: np.exp(-u**2/2) * ss.erf(c/np.sqrt(v)*np.abs(1-u/b)), -np.inf, +np.inf, args=(v,b,c), epsrel=1e-1 )[0]
+		int_exp_erf = np.vectorize( int_exp_erf, excluded=(1,2) )
+		int_v = np.vectorize( lambda b,c: sint.quad( int_exp_erf, 0, 1, args=(b,c), epsrel=1e-1 )[0] )
+		int_exp_erfc = lambda b,c: sint.quad( lambda u, b,c: np.exp(-u**2/2) * ss.erfc(c*np.abs(1-u/b)), -np.inf, +np.inf, args=(b,c), epsrel=1e-3 )[0]
+		int_exp_erfc = np.vectorize( int_exp_erfc )
+		return int_v(b,c) / int_exp_erfc(b,c) / c**2
+
+int_exp_erf = lambda b,c: sint.quad( lambda u, b,c: np.exp(-u**2/2) * ss.erf(c*np.abs(1-u/b)), -np.inf, +np.inf, args=(b,c), epsrel=1e-3 )[0]
+int_exp_erf = np.vectorize( int_exp_erf )
+
+def fpt_periodic_survival (t, rT, b, c):
+	global int_exp_erf
+	k = np.floor( t / rT )
+	if np.all(np.isinf(b)):
+		return ss.erf(c)**k * ss.erf(c*np.sqrt(rT/(t-k*rT)))
+	else:
+		int_exp_erf_kt = lambda b,c,k,t: sint.quad( lambda u, b,c,k,t: np.exp(-u**2/2) * ss.erf(c*np.abs(1-u/b)*np.sqrt(rT/(t-k*rT))), -np.inf, +np.inf, args=(b,c,k,t), epsrel=1e-3 )[0]
+		int_exp_erf_kt = np.vectorize( int_exp_erf_kt )
+		return (1/sqrt(2*π))**(k+1) * int_exp_erf(b,c)**k * int_exp_erf_kt(b,c,k,t)
+
+def fpt_periodic_disrib (t, rT, b, c):
+	global int_exp_erf
+	k = np.floor( t / rT )
+	prefact_dt_erf = lambda k,t: ((k+1)*rT-t)/sqrt(π*rT)*(t-k*rT)**(-3/2)
+	if np.all(np.isinf(b)):
+		return c * prefact_dt_erf(k,t) * ss.erf(c)**k * np.exp(-c**2*rT/(t-k*rT))
+	else:
+		int_exp_dterf = lambda b,c,k,t: sint.quad( lambda u, b,c: np.abs(1-u/b) * np.exp(-u**2/2 -c**2*(1-u/b)**2*rT/(t-k*rT)), -np.inf, +np.inf, args=(b,c,k,t), epsrel=1e-3 )[0]
+		int_exp_dterf = np.vectorize( int_exp_dterf )
+		return c * (1/sqrt(2*π))**(k+1) * int_exp_erf(b,c)**k * prefact_dt_erf(k,t) * int_exp_dterf(b,c,k,t)
--- langevin-survival.cpp
+++ langevin-survival.cpp
@@ -29,9 +29,9 @@ void* comp_thread (void* _data) {
 //	pt2_t well_center = {0.5, 0.5};
 //	_register_var(_thread, "well_k", &well_k); _register_var(_thread, "well_x", &well_center.x); _register_var(_thread, "well_y", &well_center.y);
 	
-	#define ENABLE_SURVIVAL_PROBABILITIES
-	#ifdef ENABLE_SURVIVAL_PROBABILITIES
-	// Survival probability as a function of time
+	#define ENABLE_SURVIVAL_PROBABILITIES_INTERVAL
+	#if defined(ENABLE_SURVIVAL_PROBABILITIES_DEMISPACE) || defined(ENABLE_SURVIVAL_PROBABILITIES_INTERVAL)
+	// Survival probability as a function of time at a given position
 	constexpr size_t survdist_Ndt = 100;
 	constexpr double survdist_max_t = 5.0;
 	_register_const(_thread, "survdist_max_t", survdist_max_t);
@@ -42,7 +42,7 @@ void* comp_thread (void* _data) {
 	uint64_t survdist_time_samples = 0;
 	_register_distrib(_thread, "survdist_time", survdist_time_acc, &survdist_time_samples);
 	
-	// Survival probability as a function of target position
+	// Survival probability as a function of target position at a given time
 	constexpr size_t survdist_Ndx = 400;
 	constexpr double survdist_max_x = 0.8;
 	_register_const(_thread, "survdist_max_x", survdist_max_x);
@@ -55,7 +55,7 @@ void* comp_thread (void* _data) {
 	#endif
 	
 	// First passage time at the target @ x=survdist_time_pos
-	#define FPT_JUMP_ACROSS
+//	#define FPT_JUMP_ACROSS
 	std::array<std::vector<double>,N_targets> first_times;
 	constexpr std::array<double, N_targets> first_times_xtarg = { 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50 };
 //	constexpr std::array<double, N_targets> first_times_xtarg = { 0.30 };
@@ -70,11 +70,11 @@ void* comp_thread (void* _data) {
 	
 	double t = 0;
 	size_t step = 0;
-	constexpr size_t trajectory_step_max_length = 10000000;
+	constexpr size_t trajectory_step_max_length = 1000000000;
 	size_t n_trajectories = 0;
 	_register_var(_thread, "n_trajectories", &n_trajectories);
 	
-	constexpr double Δt = 50e-6;//50 * 1.5e-6;
+	constexpr double Δt = 1e-5;//50 * 1.5e-6;
 	_register_const(_thread, "Delta_t", Δt);
 	uint8_t pause = 0;
 	_register_var(_thread, "pause", &pause);
@@ -84,12 +84,19 @@ void* comp_thread (void* _data) {
 	double init_pos_sigma = 0.1; // gaussian distribution of initial position
 	_register_var(_thread, "x0sigma", &init_pos_sigma);
 	
+	#define ENABLE_PERIODICAL_RESET
+	
 	// Poissonian resetting
-	#define ENABLE_POISSON_RESET
 	#ifdef ENABLE_POISSON_RESET
 	double reset_rate = 0.001 / Δt;
 	_register_var(_thread, "reset_rate", &reset_rate);
 	const double proba_reset_step = Δt * reset_rate;
+	#endif
+	
+	// Periodical resetting
+	#ifdef ENABLE_PERIODICAL_RESET
+	double reset_period = 1;
+	_register_var(_thread, "reset_period", &reset_period);
 	#endif
 	
 	while (not _thread.do_quit) {
@@ -98,7 +105,7 @@ void* comp_thread (void* _data) {
 			_thread.regular_callback(_thread.id_for_callback, step, t);
 		
 		::pthread_mutex_unlock(&_thread.mutex_global);
-		::usleep(pause ? 1000000 : 10);
+		::usleep(pause ? 1000000 : 100);
 		::pthread_mutex_lock(&_thread.mutex_global);
 		
 		// Let's compute one entire trajectory
@@ -127,13 +134,25 @@ void* comp_thread (void* _data) {
 		
 		double max_x_reached = 0.;
 		bool survdist_pos_done = false;
-		uint8_t target_reached = 0;
+		uint8_t target_reached = N_targets;
+		#if defined(FPT_JUMP_ACROSS) || defined(FPT_INTERVAL) || defined(FPT_DEMISPACE)
+		target_reached = 0;
+		#endif
+		
+		#ifdef ENABLE_SURVIVAL_PROBABILITIES_INTERVAL
+		std::array<bool,survdist_Ndx> survdist_survived;
+		survdist_survived.fill(true);
+		#endif
 		
 		double last_x;
 		std::array<bool,N_targets> targets_reached;
 		targets_reached.fill(false);
 		
-		#ifdef ENABLE_SURVIVAL_PROBABILITIES
+		#ifdef ENABLE_PERIODICAL_RESET
+		uint32_t n_period = 1;
+		#endif
+		
+		#if defined(ENABLE_SURVIVAL_PROBABILITIES_DEMISPACE) || defined(ENABLE_SURVIVAL_PROBABILITIES_INTERVAL)
 		while (t < survdist_max_t or (target_reached < N_targets and step < trajectory_step_max_length)) {
 		#else
 		while (target_reached < N_targets and step < trajectory_step_max_length) {
@@ -142,6 +161,12 @@ void* comp_thread (void* _data) {
 			#ifdef ENABLE_POISSON_RESET
 			if (unif01(rng) < proba_reset_step) {
 				init_pos();
+			}
+			#endif
+			#ifdef ENABLE_PERIODICAL_RESET
+			if (t > reset_period*n_period) {
+				init_pos();
+				n_period++;
 			}
 			#endif
 			last_x = x.x;
@@ -157,10 +182,10 @@ void* comp_thread (void* _data) {
 			#ifndef LANGEVIN_OVERDAMPED
 			v += a / part_m * Δt;
 			#endif
-			x = x + v * Δt;
+			x = x + v * Δt; // Δx ~= sqrt(D.Δt)
 			
 			// record x position reached
-			#if defined(ENABLE_SURVIVAL_PROBABILITIES) || defined(FPT_DEMISPACE)
+			#if defined(ENABLE_SURVIVAL_PROBABILITIES_DEMISPACE) || defined(FPT_DEMISPACE)
 			if (x.x > max_x_reached) {
 				max_x_reached = x.x;
 				#ifdef FPT_DEMISPACE
@@ -192,28 +217,53 @@ void* comp_thread (void* _data) {
 			}
 			#endif
 			
-			#ifdef ENABLE_SURVIVAL_PROBABILITIES
+			#ifdef ENABLE_SURVIVAL_PROBABILITIES_INTERVAL
+			constexpr double dx = survdist_max_x/survdist_Ndx;
+			int64_t x_k = std::floor( x.x / dx );
+			if (x_k >= 0 and x_k < survdist_Ndx)
+				survdist_survived[x_k] = false;
+			#endif
+			#if defined(ENABLE_SURVIVAL_PROBABILITIES_DEMISPACE) || defined(ENABLE_SURVIVAL_PROBABILITIES_INTERVAL)
+			
 			// let's check which targets the particle reached at specified t
 			// and fill the survival distribution as a function of target position accordingly
 			if (t > survdist_pos_time and not survdist_pos_done) {
+				#ifdef ENABLE_SURVIVAL_PROBABILITIES_DEMISPACE
 				constexpr double dx = survdist_max_x/survdist_Ndx;
 				int64_t x_k = std::floor( max_x_reached / dx );
 				for (size_t k = 0; k < survdist_Ndx; k++) {
-					if (k >= x_k) // survived all targets at < x_k
+					if (k >= x_k) // survived all targets at < x_k; only compatible with particule resetting at x=0 (no backwards target reaching)
 						survdist_pos_acc[k]++;
 				}
+				#endif
+				#ifdef ENABLE_SURVIVAL_PROBABILITIES_INTERVAL
+				for (size_t k = 0; k < survdist_Ndx; k++) {
+					if (survdist_survived[k])
+						survdist_pos_acc[k]++;
+				}
+				#endif
 				survdist_pos_samples++;
 				survdist_pos_done = true;
 			}
 			
+			//
 			constexpr double dt = survdist_max_t/survdist_Ndt;
 			uint64_t t_k = std::floor( t / dt );
 			if (t_k < survdist_Ndt) {
+				#ifdef ENABLE_SURVIVAL_PROBABILITIES_DEMISPACE
 				if (max_x_reached < survdist_time_pos)  // survived targets at survdist_time_pos
 					survdist_time_acc[t_k]++;
+				#endif
+				#ifdef ENABLE_SURVIVAL_PROBABILITIES_INTERVAL
+				constexpr size_t x_k = survdist_time_pos/dx;
+				if (survdist_survived[x_k])
+					survdist_time_acc[t_k]++;
+				#endif
 				survdist_time_samples++;
 			}
+			
 			#endif
+
 			
 			t += Δt;
 			step++;
